4d3eeac3d098cecc5003918debf95dcdd914adb8
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-08-05 15:23:29 +0530

    Reorder commands in build.sh for migration

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_test_users
-python manage.py seed_ai_data
-python manage.py train_risk_model
