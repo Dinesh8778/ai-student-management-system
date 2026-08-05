7195a02ed10dccc64b6c981bee35306f7a282c05
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-08-05 15:15:07 +0530

    Point at Neon database, re-seed
    Render DB has expired pointed to Neon DB

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+bash
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_test_users
+python manage.py seed_ai_data
+python manage.py train_risk_model
